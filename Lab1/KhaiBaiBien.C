e2b328338f05019959ceb2cfe5d098e3e4912a86
Pham Minh Tien <phamminhtien3344@gmail.com>
2025-11-07 14:30:11 +0700

    nop bai lab1

--- Lab1/KhaiBaiBien.C
+++ Lab1/KhaiBaiBien.C
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
+    int a, b;
     printf("hello, chao cac ban");
+    printf("\nhap vao a va b: ");
+    scanf("%d %d", &a,&b);
+    prinft("ban vua nhap a = %d, b = %d\n", a, b);
     return 0;
 
 }
